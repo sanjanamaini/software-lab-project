f443c24dcf0168dab6221a6da953debac1779d17
Sanjana Maini <sanjumaini18@gmail.com>
2024-04-29 11:56:01 +0530

    backend done and changed navbar css

--- backend/server.js
+++ backend/server.js
@@ -37,22 +37,106 @@ app.get("/api/login", async (req, res) => {
 
 app.get("/api/all/room/details", async (req, res) => {
     try {
-        console.log(req.headers);
-        const { room_number, tower, capacity, occupied } = req.headers;
+        // console.log(req.headers);
+        // const { room_number, tower, capacity, occupied } = req.headers;
         // console.log(registrationnum, password);
         const query = {
-            text: "SELECT * FROM rooms WHERE room_number=$1 AND tower=$2 AND capacity=$3 AND  occupied=$4" ,
-            values: [room_number, tower, capacity, occupied],
+            text: "SELECT * FROM rooms  "
             
         };
-        console.log(query);
+        // console.log(query);
         const { rows } = await pool.query(query);
-        console.log(rows);
+        // console.log(rows);
         res.json(rows);
     } catch (err) {
         console.error(err);
         res.status(500).json({ status: "error", message: err.message });
     }
+});
+
+app.post("/api/room/update", async (req, res) => {
+    try {
+        const { registrationnum, tower, floor, room } = req.headers;
+
+        const updateStudentQuery = {
+            text: "UPDATE students SET tower = $1, floor = $2, room = $3 WHERE registrationnum = $4;",
+            values: [tower, floor, room, registrationnum],
+        };
+
+        await pool.query(updateStudentQuery);
+        // roomnumber = room;
+        const updateRoomQuery = {
+            text: "UPDATE rooms SET occupied = $1, studentregno = $2 WHERE roomnumber = $3 AND tower = $4 AND floor = $5;",
+            values: [1, registrationnum, room, tower, floor],
+        };
+
+        await pool.query(updateRoomQuery);
+
+        res.status(200).json({ message: "Room updated successfully" });
+    } catch (error) {
+        console.error("Error updating room:", error);
+        res.status(500).json({ error: "Error updating room" });
+    }
+});
+
+
+app.get("/api/fetch/booking", async (req, res) => {
+	try {
+		// console.log("in for item check");
+		const { registrationnum } = req.headers;
+		const query = {
+			text: "SELECT * FROM students WHERE registrationnum = $1",
+			values: [registrationnum],
+		};
+		const { rows } = await pool.query(query);
+		res.json(rows);
+	} catch (error) {
+		console.error("Error executing query:", error);
+		res.status(500).json({ error: "Error executing query" });
+	}
+});
+
+app.post("/api/fine", async (req, res) => {
+    try {
+        const { registrationnum, amt, rsn } = req.headers;
+        const query = {
+            text: "INSERT INTO fines (regno,amt,rsn) VALUES($1,$2,$3)",
+            values: [registrationnum, amt, rsn],
+
+        };
+        await pool.query(query);
+    }catch (error) {
+		console.error("Error executing query:", error);
+		res.status(500).json({ error: "Error executing query" });
+	}
+});
+
+app.post("/api/add/student", async (req, res) => {
+    try {
+        const { name, registrationnum, department, semester, cgpa, yearofstudy } = req.headers;
+        const query = {
+            text: "INSERT INTO students ( name, registrationnum, department, semester, cgpa, yearofstudy)  VALUES ($1, $2, $3,$4,$5,$6)",
+            values: [name, registrationnum, department, semester, cgpa, yearofstudy],
+        };
+        await pool.query(query);
+    }catch (error) {
+		console.error("Error executing query:", error);
+		res.status(500).json({ error: "Error executing query" });
+	}
+});
+
+app.post("/api/del/student", async (req, res) => {
+    try {
+        const { registrationnum } = req.headers;
+        const query = {
+            text: "DELETE FROM students WHERE registrationnum= $1 ",
+            values: [ registrationnum],
+        };
+        await pool.query(query);
+    }catch (error) {
+		console.error("Error executing query:", error);
+		res.status(500).json({ error: "Error executing query" });
+	}
 });
 
 app.listen(port, () => {
--- js/student.js
+++ js/student.js
@@ -1,146 +1,332 @@
-window.onload = function() {
-    var userDetsString = localStorage.getItem("currentUserDetails");
-    var userDets = JSON.parse(userDetsString);
-    console.log(userDets);
-    if (userDets[0].registrationnum.substring(0, 2) == "AP") {
-        console.log("student view 1");
-        SView(userDets);
-        console.log("student view 2s")
-    } else if(userDets[0].registrationnum=="Admin"){
-        AView();
-    } else if(userDets[0].registrationnum.substring(0,1)=="W"){
-        WView();
-    }
-    hideAndUnhide();
+var userDets;
+
+window.onload = function () {
+	var userDetsString = localStorage.getItem("currentUserDetails");
+	userDets = JSON.parse(userDetsString);
+	console.log(userDets);
+	if (userDets[0].registrationnum.substring(0, 2) == "AP") {
+		console.log("student view 1");
+		SView(userDets);
+		console.log("student view 2s");
+	} else if (userDets[0].registrationnum == "Admin") {
+		AView();
+	} else if (userDets[0].registrationnum.substring(0, 1) == "W") {
+		WView();
+	}
+	hideAndUnhide();
 };
 
 function hideAndUnhide() {
-    var inactiveDivs = document.getElementsByClassName('inactiveView');
-    var activeDivs=document.getElementsByClassName('activeView');
+	var inactiveDivs = document.getElementsByClassName("inactiveView");
+	var activeDivs = document.getElementsByClassName("activeView");
 
-    for (var i = 0; i < inactiveDivs.length; i++) {
-        inactiveDivs[i].style.display = 'none';
-    }
-    for (let i = 0; i < activeDivs.length; i++) {
-        activeDivs[i].style.display=''
-    }
+	for (var i = 0; i < inactiveDivs.length; i++) {
+		inactiveDivs[i].style.display = "none";
+	}
+	for (let i = 0; i < activeDivs.length; i++) {
+		activeDivs[i].style.display = "";
+	}
 }
 
 function setActive(event, view) {
-    var clickedID = event.target.id; // Get the ID of the clicked element
-    document.getElementsByClassName('active')[0].classList.remove('active'); // Get all elements with 'activeView' class
-    document.getElementsByClassName('activeView')[0].classList.add('inactiveView');
-    document.getElementsByClassName('activeView')[0].classList.remove('activeView');
+	var clickedID = event.target.id; // Get the ID of the clicked element
+	document.getElementsByClassName("active")[0].classList.remove("active"); // Get all elements with 'activeView' class
+	document
+		.getElementsByClassName("activeView")[0]
+		.classList.add("inactiveView");
+	document
+		.getElementsByClassName("activeView")[0]
+		.classList.remove("activeView");
 
-    document.getElementById(clickedID).classList.add('active');
-    document.getElementById(view).classList.remove('inactiveView'); // Remove the 'inactiveView' class
-    document.getElementById(view).classList.add('activeView');
-    hideAndUnhide();
+        document.getElementById(clickedID).classList.add("active");
+        document.getElementById(view).classList.remove("activeView"); // Remove the 'inactiveView' class
+        document.getElementById(view).classList.add("activeView");
+	hideAndUnhide();
 }
 
 function SView(userDets) {
-    removeAddStudent();
-    removeDeleteStudent();
-    removeCollectFine();
-    removeAllRoomDetails();
+	removeAddStudent();
+	removeDeleteStudent();
+	removeCollectFine();
+	removeAllRoomDetails();
 
-    document.getElementById('studentName').innerText = userDets[0].name;
-    document.getElementById('studentID').innerText = userDets[0].registrationnum;
-    document.getElementById('studentDept').innerText = userDets[0].department;
-    document.getElementById('studentCGPA').innerText = userDets[0].cgpa;
-    document.getElementById('studentYear').innerText = userDets[0].yearofstudy;
+	document.getElementById("studentName").innerText = userDets[0].name;
+	document.getElementById("studentID").innerText = userDets[0].registrationnum;
+	document.getElementById("studentDept").innerText = userDets[0].department;
+	document.getElementById("studentCGPA").innerText = userDets[0].cgpa;
+	document.getElementById("studentYear").innerText = userDets[0].yearofstudy;
     // document.getElementById('studentName').innerText = userDets[0].name;
     
-
+    document.getElementById('RoomNo').innerText=userDets[0].room;
+    document.getElementById('FloorNo').innerText=userDets[0].floor;
+    document.getElementById('TowerNo').innerText=userDets[0].tower;
 }
 
-
 function WView() {
-    removeAddStudent();
-    removeBookingDetails();
-    removePayment();
-    removeDeleteStudent();
-    removeStudentDets();
-    removeSelectRoom();
-    removeRoomDetails();
-    document.getElementById('studentView').classList.add("inactiveView");
-    document.getElementById('AllroomDetailsView').classList.add("activeView");
-    document.getElementById('studentView').classList.remove("activeView");
-    hideAndUnhide();
+	removeAddStudent();
+	removeBookingDetails();
+	// removePayment();
+	removeDeleteStudent();
+	removeStudentDets();
+	removeSelectRoom();
+	// removeRoomDetails();
+	document.getElementById("studentView").classList.add("inactiveView");
+	document.getElementById("AllroomDetailsView").classList.add("activeView");
+	document.getElementById("studentView").classList.remove("activeView");
+	hideAndUnhide();
 }
 
 function AView() {
-    removeBookingDetails();
-    removePayment();
-    removeStudentDets();
-    removeSelectRoom();
-    removeRoomDetails();
-    removeCollectFine();
-    removeAllRoomDetails();
-    document.getElementById('studentView').classList.add("inactiveView");
-    document.getElementById('AllroomDetailsView').classList.add("activeView");
-    document.getElementById('studentView').classList.remove("activeView");
-    hideAndUnhide();
+	removeBookingDetails();
+	// removePayment();
+	removeStudentDets();
+	removeSelectRoom();
+	// removeRoomDetails();
+	removeCollectFine();
+	removeAllRoomDetails();
+	document.getElementById("studentView").classList.add("inactiveView");
+	document.getElementById("addStudentView").classList.add("activeView");
+	document.getElementById("studentView").classList.remove("activeView");
+	hideAndUnhide();
 }
 
 function removeStudentDets() {
-    document.getElementById("student").style.display='none';
+	document.getElementById("student").style.display = "none";
 }
 
-function removePayment(){
-    document.getElementById("makpayment").style.display='none';
+// function removePayment() {
+// 	document.getElementById("makpayment").style.display = "none";
+// }
+
+function removeSelectRoom() {
+	document.getElementById("SelRoom").style.display = "none";
 }
 
-function removeSelectRoom(){
-    document.getElementById("SelRoom").style.display='none';
-}
-
-function removeRoomDetails(){
-    document.getElementById("details").style.display='none';
-}
+// function removeRoomDetails() {
+// 	document.getElementById("details").style.display = "none";
+// }
 
 function removeAllRoomDetails() {
-    document.getElementById("AllroomDetailsView").style.display = "none";
+	document.getElementById("allroomdetails").style.display = "none";
 }
 
-function removeBookingDetails(){
-    document.getElementById("bookingDetails").style.display='none';
+function removeBookingDetails() {
+	document.getElementById("bookingDetails").style.display = "none";
 }
 
-function removeCollectFine(){
-    document.getElementById('paymentDetails').style.display='none';
+function removeCollectFine() {
+	document.getElementById("paymentDetails").style.display = "none";
 }
-function removeAddStudent(){
-    document.getElementById('addStudent').style.display='none';
+function removeAddStudent() {
+	document.getElementById("addStudent").style.display = "none";
 }
-function removeDeleteStudent(){
-    document.getElementById('deletestudent').style.display='none';
+function removeDeleteStudent() {
+	document.getElementById("deletestudent").style.display = "none";
 }
 
 function isValidName(name) {
-    if (name.length > 0 && name.length <= 50) {
-        return /^[a-zA-Z\s'-]+$/.test(name);
-    } else {
-        return false; // Return false if the name is empty or exceeds 50 characters
-    }
+	if (name.length > 0 && name.length <= 50) {
+		return /^[a-zA-Z\s'-]+$/.test(name);
+	} else {
+		return false; // Return false if the name is empty or exceeds 50 characters
+	}
 }
 
 function validateName() {
-    var nameInput = document.getElementById("newUserFullName");
-    var validationMessage = document.getElementById("nameInvalid");
+	var nameInput = document.getElementById("newUserFullName");
+	var validationMessage = document.getElementById("nameInvalid");
 
-    if (isValidName(nameInput.value)) {
-        validationMessage.innerText = "";
-        return true;
-    } else {
-        validationMessage.innerText = "Invalid Name";
-        return false;
-    }
+	if (isValidName(nameInput.value)) {
+		validationMessage.innerText = "";
+		return true;
+	} else {
+		validationMessage.innerText = "Invalid Name";
+		return false;
+	}
 }
 
 function logoutandclearstorage() {
-    sessionStorage.clear();
-    localStorage.clear();
-    window.location.href = "index.html";
+	sessionStorage.clear();
+	localStorage.clear();
+	window.location.href = "index.html";
 }
+
+//Creating room selection
+async function createRoomSelection() {
+	const id = userDets[0].registrationnum;
+	const tower = document.getElementById("setTower").value;
+	const floor = document.getElementById("setFloor").value;
+	const roomNo = document.getElementById("setRoom").value;
+	console.log(id, tower, floor, roomNo);
+	document.getElementById("roomSel").innerText = "Updating ...";
+	const detailUpdateStatus = await updateRoomDetails(
+		id,
+		tower,
+		floor,
+		roomNo
+	);
+}
+
+const updateRoomDetails = async (registrationnum, tower, floor, room) => {
+	console.log(registrationnum, tower, floor, room);
+	const response = await fetch("http://localhost:3000/api/room/update", {
+		method: "POST",
+		headers: {
+			registrationnum: registrationnum,
+			tower: tower,
+			floor: floor,
+			room: room,
+		},
+    });
+    const res = await response.json();
+    document.getElementById('roomSel').innerText = 'Updated Successfully!';
+};
+
+// fetching booking details from DB
+async function fetchRoomSelection() {
+	const id = userDets[0].registrationnum;
+	
+	// console.log(id, tower, floor, roomNo);
+	document.getElementById("submitReq").innerText = "Fetching ...";
+    const detailUpdateStatus = await fetchRoomDetails(id);
+    console.log(detailUpdateStatus);
+    userDets[0] = detailUpdateStatus[0];
+    document.getElementById('RoomNo').innerText=userDets[0].room;
+    document.getElementById('FloorNo').innerText=userDets[0].floor;
+    document.getElementById('TowerNo').innerText=userDets[0].tower;
+
+}
+
+const fetchRoomDetails = async (registrationnum) => {
+	// console.log(registrationnum);
+	const response = await fetch("http://localhost:3000/api/fetch/booking", {
+		method: "GET",
+		headers: {
+			registrationnum: registrationnum
+		},
+    });
+    const res = await response.json();
+    document.getElementById('submitReq').innerText = 'Fetched Successfully!';
+    return res;
+};
+
+
+async function fetchAllRooms() {
+    try {
+        const response = await fetch("http://localhost:3000/api/all/room/details");
+        const data = await response.json();
+        const tableBody = document.getElementById("bookingData");
+        
+        // Clear previous items
+        console.log(data);
+        tableBody.innerHTML = "";
+
+        data.forEach((item) => {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                <td>${item.roomnumber}</td>
+                <td>${item.floor}</td>
+                <td>${item.tower}</td>
+                <td>${item.occupied}</td>
+                <td>${item.studentregno}</td>
+            `;
+            tableBody.appendChild(row);
+        });
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+//collecting fine
+async function collectFine() {
+    const id = document.getElementById("fineRegNo").value;
+    const amount = document.getElementById("fineAmt").value;
+    const reason = document.getElementById("finersn").value;
+    console.log(id, amount, reason);
+    
+    const sendFine = await updateFine(
+		id,
+		amount,
+		reason
+	);
+}
+const updateFine = async (id, amount, reason) => {
+	// console.log(registrationnum, tower, floor, room);
+	const response = await fetch("http://localhost:3000/api/fine", {
+		method: "POST",
+		headers: {
+			registrationnum: id,
+			amt: amount,
+			rsn: reason,
+		},
+    });
+    const res = await response.json();
+    document.getElementById('detailsConfirm').innerText = 'Sent Successfully!';
+};
+
+//add student
+async function addStudent() {
+    const name = document.getElementById("addStudName").value;
+    const id = document.getElementById("addregno").value;
+    const dept = document.getElementById("addDept").value;
+    const sem = document.getElementById("addSem").value;
+    const cgpa = document.getElementById("addCgpa").value;
+    const year = document.getElementById("addYear").value;
+    // console.log(id, amount, reason);
+    document.getElementById('addStudentConfirm').innerText="Adding Student...";
+    console.log(name,id,dept,sem,cgpa,year);
+    
+    const addstud = await updateAddStud(
+        name,
+        id,
+		dept,
+        sem,
+        cgpa,
+        year
+    );
+    // document.getElementById('addStudentConfirm').innerText="Added Successfully!";
+
+}
+const updateAddStud = async (name,id,dept,sem,cgpa,year) => {
+	// console.log(registrationnum, tower, floor, room);
+	const response = await fetch("http://localhost:3000/api/add/student", {
+		method: "POST",
+        headers: {
+            name:name,
+			registrationnum: id,
+			department: dept,
+            semester: sem,
+            cgpa: cgpa,
+            yearofstudy:year,
+		},
+    });
+    const res = await response.json();
+    document.getElementById('addStudentConfirm').innerText="Added Successfully!";
+
+};
+
+
+//delete student
+async function delStudent() {
+    const id = document.getElementById("delregno").value;
+    console.log(id);
+
+    document.getElementById('addStudentConfirm').innerText="Deleting Student...";
+
+    const del = await updateDelStud(
+		id
+    );
+    
+}
+const updateDelStud = async (id) => {
+	// console.log(registrationnum, tower, floor, room);
+	const response = await fetch("http://localhost:3000/api/del/student", {
+		method: "POST",
+		headers: {
+			registrationnum: id,
+		},
+    });
+    const res = await response.json();
+    document.getElementById('delStudentConfirm').innerText = 'Deleted Successfully!';
+};
 
